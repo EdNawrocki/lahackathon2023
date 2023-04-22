2d83bf09f9a0b2214a56ab8f8632a69ad58368c7
Henry Nguyen <henryvnguyen57@gmail.com>
2023-04-22 12:03:05 -0500

    implemented speechToText
    not styled yet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import SpeechToText from './components/SpeechToText/SpeechToText'
 
 function App() {
   const [data, setData] = useState([{}])
@@ -18,9 +19,10 @@ function App() {
     <div>
       {(typeof data.end === 'undefined') ? (
         <p>Loading...</p>
-      ): (
+      ) : (
         <p>{data.end}</p>
       )}
+      <SpeechToText/>
     </div>
   )
 }
